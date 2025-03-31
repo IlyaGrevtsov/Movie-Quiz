6d0a652dbfeb1a12c168da2f317d848de8db496f
IlyaGrevtsov <ilyagrevtsov00@gmail.com>
2025-03-31 15:33:44 +0300

    Сделаны мок данные, добавлена связь сториборда с кодом

---
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,13 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Илья on 31.03.2025.
+//
+import Foundation
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,10 +1,72 @@
 import UIKit
 
+
 final class MovieQuizViewController: UIViewController {
+    // MARK: - Button Action
+
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func yesButtonClicket(_ sender: UIButton) {
+    
+    }
+    
+    @IBOutlet private weak var counterLabel: UILabel!
+    
+    @IBOutlet private weak var textLabel: UILabel!
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
     // MARK: - Lifecycle
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+    
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+ 
 }
 
 /*
