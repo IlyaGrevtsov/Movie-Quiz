a3fceacf2b959c87b35ca4e23caf07a7c1d591fb
IlyaGrevtsov <ilyagrevtsov00@gmail.com>
2025-04-01 15:26:56 +0300

    Реализована логика по макету, добавлен алерт с подсчетом правильных ответов

--- MovieQuiz/Models/QuizQuestion.swift
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -5,9 +5,20 @@
 //  Created by Илья on 31.03.2025.
 //
 import Foundation
+import UIKit
 
 struct QuizQuestion {
     let image: String
     let text: String
     let correctAnswer: Bool
+}
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
+struct QuizResultViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
 }
